a6ca7b683b924ac5d58270f38b6da9f7b0e1ef35
CyraShane <144327118+CyraShane@users.noreply.github.com>
2024-12-01 17:34:34 +0800

    202110477 Perez

---
+++ ETHAVAX.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SimpleToken is ERC20, Ownable {
+    constructor() ERC20("SimpleToken", "STK") {
+        _mint(msg.sender, 100 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
