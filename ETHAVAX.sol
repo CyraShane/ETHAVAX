0af809f904cf5cc8bbc50ba97d6af502a6f83dab
CyraShane <144327118+CyraShane@users.noreply.github.com>
2024-12-01 19:21:02 +0800

    202110477 Perez

--- ETHAVAX.sol
+++ ETHAVAX.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SimpleToken is ERC20, Ownable {
-    constructor() ERC20("SimpleToken", "STK") {
+    constructor() ERC20("SimpleToken", "STK") Ownable(msg.sender) {
         _mint(msg.sender, 100 * 10 ** decimals());
     }
 
